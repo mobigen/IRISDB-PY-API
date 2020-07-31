a9e72ca09bf1894a6c87cd682fd5ab25a59d80a6
ollehq <vkdkf55@naver.com>
2020-07-31 11:58:31 +0900

    setup upload

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='M6',
+    version="1.0"
+    description="M6",
+    long_description="""
+        IRISDB-PY-API
+    """,
+    license="BSD (3-clause)",
+    author="Min RK",
+    author_email="benjaminrk@gmail.com",
+    url="https://github.com/mobigen/IRISDB-PY-API",
+)

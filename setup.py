da5f9827348a0b9b8482ab99b3c28c12600caba3
안형민(An, hyoung-min) <wakehm@gmail.com>
2021-03-09 18:20:49 +0900

    pip install build 이슈 처리 (#1)
    * pip install build issue
    * fix version 2.2.0

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
     name='M6',
-    version="1.0",
+    version="2.2.0",
     description="M6",
     long_description="""
         IRISDB-PY-API
     """,
+    zip_safe=False,
+    packages=find_packages(exclude=('tests',)),
     url="https://github.com/mobigen/IRISDB-PY-API",
 )

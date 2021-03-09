bbc5f8a0740714b1c7799408d6238f9af0d9e38b
anhm <wakehm@gmail.com>
2021-03-09 18:18:20 +0900

    fix version 2.2.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='M6',
-    version="1.0",
+    version="2.2.0",
     description="M6",
     long_description="""
         IRISDB-PY-API
